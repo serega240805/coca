f4f255118a909388eef36e8cea174e3be38277ab
serega240805 <ssuchalkin@yandex.ru>
2024-04-14 13:53:01 +0300

    Add mixins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,23 +3,23 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-    'prettier/prettier': 'error',
+    "prettier/prettier": "error",
   },
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,1 @@
-import '/scss/main.scss';
-
-// components
-// import { useTheme } from './components/theme.js';
-// import { useBurger } from './components/burger.js';
-// import {
-//   useInsightSlider,
-//   useTestimonialsSlider,
-// } from './components/home/slider.js';
-
-// useTheme();
-// useBurger();
-// useInsightSlider();
-// useTestimonialsSlider();
+import "/scss/main.scss";
