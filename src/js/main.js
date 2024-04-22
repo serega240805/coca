f1b65b4eaf6165c772de8e4b138c21a4c3656e55
serega240805 <ssuchalkin@yandex.ru>
2024-04-22 22:00:00 +0300

    Created insight section and added swiper

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import "/scss/main.scss";
 // components
 import { useTheme } from "./components/theme.js";
 import { useBurger } from "./components/burger.js";
+import { useInsightSlider } from "./components/home/slider.js";
 
 useTheme();
 useBurger();
+useInsightSlider();
