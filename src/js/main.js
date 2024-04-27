fb5e4cc42c38e016d5f5aea56a558a262defbefa
serega240805 <ssuchalkin@yandex.ru>
2024-04-27 14:50:22 +0300

    Created testimonials section

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,9 @@ import "/scss/main.scss";
 import { useTheme } from "./components/theme.js";
 import { useBurger } from "./components/burger.js";
 import { useInsightSlider } from "./components/home/slider.js";
+import { useTestimonialsSlider } from "./components/home/slider.js";
 
 useTheme();
 useBurger();
 useInsightSlider();
+useTestimonialsSlider();
