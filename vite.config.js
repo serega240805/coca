230feb53ae445f47207c78618ab8dd1064579635
serega240805 <ssuchalkin@yandex.ru>
2024-05-01 19:26:02 +0300

    Created contact:hero and added contact:partners

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
       input: {
         main: "./src/index.html",
         pricing: "./src/pricing.html",
+        contact: "./src/contact.html",
       },
     },
   },
