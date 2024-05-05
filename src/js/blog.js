679a9b37f6ca50e2f1a5db71d7f6d73896451373
serega240805 <ssuchalkin@yandex.ru>
2024-05-05 18:58:36 +0300

    Created blog:articles section

--- src/js/blog.js
+++ src/js/blog.js
@@ -3,10 +3,11 @@ import "/scss/blog.scss";
 // components
 import { useTheme } from "./components/theme.js";
 import { useBurger } from "./components/burger.js";
-import { useHeroSlider } from "./components/blog/slider.js";
+import { useHeroSlider, useArticlesSlider } from "./components/blog/slider.js";
 import { useTabs } from "./components/blog/tabs.js";
 
 useTheme();
 useBurger();
 useHeroSlider();
+useArticlesSlider();
 useTabs();
--- src/js/components/blog/slider.js
+++ src/js/components/blog/slider.js
@@ -1,4 +1,5 @@
 import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 
 export const useHeroSlider = () => {
@@ -11,6 +12,19 @@ export const useHeroSlider = () => {
       993: {
         centeredSlides: false,
       },
+    },
+  });
+};
+
+export const useArticlesSlider = () => {
+  new Swiper('.articles__slider', {
+    modules: [Navigation],
+    slidesPerView: 'auto',
+    spaceBetween: 32,
+    loop: true,
+    navigation: {
+      prevEl: '.articles__btn--prev',
+      nextEl: '.articles__btn--next',
     },
   });
 };
