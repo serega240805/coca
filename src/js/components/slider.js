490bf2eafa5bb4818d921238d66a6b49a9efba8f
serega240805 <ssuchalkin@yandex.ru>
2024-05-06 21:08:43 +0300

    Update partner section and add favicon

---
+++ src/js/components/slider.js
@@ -0,0 +1,17 @@
+import Swiper from 'swiper';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+
+export const usePartnersSlider = () => {
+    Swiper.use([Autoplay])
+  new Swiper('.partners__slider', {
+    slidesPerView: 'auto',
+    spaceBetween: 24,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: true,
+    },
+  });
+};
--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,12 @@ import "/scss/main.scss";
 import { useTheme } from "./components/theme.js";
 import { useBurger } from "./components/burger.js";
 import { headerVisible } from "./components/header.js";
+import { usePartnersSlider } from "./components/slider.js";
 import { useInsightSlider, useTestimonialsSlider } from "./components/home/slider.js";
 
 useTheme();
 useBurger();
 headerVisible();
+usePartnersSlider();
 useInsightSlider();
 useTestimonialsSlider();
